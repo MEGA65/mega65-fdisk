e4f2e3ff096d5601a575cd5a3df9c89d0aa1eedb
Paul Gardner-Stephen <paul@servalproject.org>
2018-01-21 17:15:23 +1030

    use new enhanced dma

--- fdisk_memory.c
+++ fdisk_memory.c
@@ -9,6 +9,15 @@
 
 
 struct dmagic_dmalist {
+  // Enhanced DMA options
+  unsigned char option_0b;
+  unsigned char option_80;
+  unsigned char source_mb;
+  unsigned char option_81;
+  unsigned char dest_mb;
+  unsigned char end_of_options;
+
+  // F018B format DMA request
   unsigned char command;
   unsigned int count;
   unsigned int source_addr;
@@ -22,18 +31,15 @@ struct dmagic_dmalist {
 struct dmagic_dmalist dmalist;
 unsigned char dma_byte;
 
-void do_dma(long source_address,long destination_address)
+void do_dma(void)
 {
   m65_io_enable();
   
   // Now run DMA job (to and from anywhere, and list is in low 1MB)
-  POKE(0xd703U,1); // enable F018B mode
   POKE(0xd702U,0);
-  POKE(0xd704U,0);
-  POKE(0xd705U,source_address>>20);
-  POKE(0xd706U,destination_address>>20);
+  POKE(0xd704U,0x00);  // List is in $00xxxxx
   POKE(0xd701U,((unsigned int)&dmalist)>>8);
-  POKE(0xd700U,((unsigned int)&dmalist)&0xff); // triggers DMA
+  POKE(0xd705U,((unsigned int)&dmalist)&0xff); // triggers enhanced DMA
 }
 
 unsigned char lpeek(long address)
@@ -42,6 +48,14 @@ unsigned char lpeek(long address)
   // XXX - Optimise out repeated setup etc
   // (separate DMA lists for peek, poke and copy should
   // save space, since most fields can stay initialised).
+
+  dmalist.option_0b=0x0b;
+  dmalist.option_80=0x80;
+  dmalist.source_mb=(address>>20);
+  dmalist.option_81=0x81;
+  dmalist.dest_mb=0x00; // dma_byte lives in 1st MB
+  dmalist.end_of_options=0x00;
+  
   dmalist.command=0x00; // copy
   dmalist.count=1;
   dmalist.source_addr=address&0xffff;
@@ -49,13 +63,21 @@ unsigned char lpeek(long address)
   dmalist.dest_addr=(unsigned int)&dma_byte;
   dmalist.dest_bank=0;
 
-  do_dma(address,0);
+  do_dma();
    
   return dma_byte;
 }
 
 void lpoke(long address, unsigned char value)
 {  
+
+  dmalist.option_0b=0x0b;
+  dmalist.option_80=0x80;
+  dmalist.source_mb=0x00; // dma_byte lives in 1st MB
+  dmalist.option_81=0x81;
+  dmalist.dest_mb=(address>>20);
+  dmalist.end_of_options=0x00;
+  
   dma_byte=value;
   dmalist.command=0x00; // copy
   dmalist.count=1;
@@ -64,13 +86,20 @@ void lpoke(long address, unsigned char value)
   dmalist.dest_addr=address&0xffff;
   dmalist.dest_bank=(address>>16)&0x7f;
 
-  do_dma(0,address); 
+  do_dma(); 
   return;
 }
 
 void lcopy(long source_address, long destination_address,
 	  unsigned int count)
 {
+  dmalist.option_0b=0x0b;
+  dmalist.option_80=0x80;
+  dmalist.source_mb=source_address>>20;
+  dmalist.option_81=0x81;
+  dmalist.dest_mb=(destination_address>>20);
+  dmalist.end_of_options=0x00;
+
   dmalist.command=0x00; // copy
   dmalist.count=count;
   dmalist.sub_cmd=0;
@@ -83,13 +112,20 @@ void lcopy(long source_address, long destination_address,
   if (destination_address>=0xd000 && destination_address<0xe000)
     dmalist.dest_bank|=0x80;
 
-  do_dma(source_address,destination_address);
+  do_dma();
   return;
 }
 
 void lfill(long destination_address, unsigned char value,
 	  unsigned int count)
 {
+  dmalist.option_0b=0x0b;
+  dmalist.option_80=0x80;
+  dmalist.source_mb=0x00;
+  dmalist.option_81=0x81;
+  dmalist.dest_mb=destination_address>>20;
+  dmalist.end_of_options=0x00;
+
   dmalist.command=0x03; // fill
   dmalist.sub_cmd=0;
   dmalist.count=count;
@@ -99,7 +135,7 @@ void lfill(long destination_address, unsigned char value,
   if (destination_address>=0xd000 && destination_address<0xe000)
     dmalist.dest_bank|=0x80;
 
-  do_dma(0,destination_address);
+  do_dma();
   return;
 }
 
