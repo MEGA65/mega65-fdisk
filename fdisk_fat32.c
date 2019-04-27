7ae16b90575a8c2944b98b6d97e3e600520579f7
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-27 12:06:58 +0930

    work on making contiguous file allocation routine more general

--- fdisk_fat32.c
+++ fdisk_fat32.c
@@ -27,47 +27,76 @@ long fat32_create_contiguous_file(char *name, long size,
   unsigned long start_cluster=0;
   unsigned long next_cluster;
   unsigned long contiguous_clusters=0;
+  unsigned int fat_offset=0;
   char message[40]="Found file: ????????.???";
 
   clusters=size/4096;
 
-  sdcard_readsector(fat1_sector);
-  for(offset=0;offset<512;offset+=4)
-    {
-      next_cluster=sector_buffer[offset];
-      next_cluster|=((long)sector_buffer[offset+1]<<8L);
-      next_cluster|=((long)sector_buffer[offset+2]<<16L);
-      next_cluster|=((long)sector_buffer[offset+3]<<24L);
+  for(fat_offset=0;fat_offset <= (fat2_sector-fat1_sector); fat_offset++) {
+    sdcard_readsector(fat1_sector+fat_offset);
+    contiguous_clusters=0;
+    start_cluster=0;
+
+    // Skip any FAT sectors with allocated clusters
+    int j;
+    for(j=0;j<512;j++) if (sector_buffer[j]) break;
+    if (j!=512) {
+      printf("FAT sector #%d\n",fat_offset);
+      for(j=0;j<512;j++)
+	{
+	  printf(" %02x",sector_buffer[j]);
+	  if ((j&0x1f)==0x1f) printf("\n");
+	}
+      continue;
+    }
+    
+    for(offset=0;offset<512;offset+=4)
+      {
+	next_cluster=sector_buffer[offset];
+	next_cluster|=((long)sector_buffer[offset+1]<<8L);
+	next_cluster|=((long)sector_buffer[offset+2]<<16L);
+	next_cluster|=((long)sector_buffer[offset+3]<<24L);
 #ifdef __CC65__
-      screen_decimal(screen_line_address-80+8,offset/4);
-      screen_hex(screen_line_address-80+32,next_cluster);
+	screen_decimal(screen_line_address-80+8,offset/4);
+	screen_hex(screen_line_address-80+32,next_cluster);
 #endif
-      if (!next_cluster) {
-	if (!start_cluster) {
-	  start_cluster=offset/4;
-	}
-	contiguous_clusters++;
-	if (contiguous_clusters==clusters) {
-	  // End of chain marker
-	  sector_buffer[offset+0]=0xff; sector_buffer[offset+1]=0xff;
-	  sector_buffer[offset+2]=0xff; sector_buffer[offset+3]=0x0f;
-	  break;
+	if (!next_cluster) {
+	  if (!start_cluster) {
+	    start_cluster=(offset/4)+fat_offset*(512/4);
+	  }
+	  contiguous_clusters++;
+	  if (contiguous_clusters==clusters) {
+	    // End of chain marker
+	    sector_buffer[offset+0]=0xff; sector_buffer[offset+1]=0xff;
+	    sector_buffer[offset+2]=0xff; sector_buffer[offset+3]=0x0f;
+	    break;
+	  } else {
+	    // Point to next cluster
+	    sector_buffer[offset+0]=(offset/4)+1;
+	    sector_buffer[offset+1]=0; sector_buffer[offset+2]=0; sector_buffer[offset+3]=0;	  
+	  }
 	} else {
-	  // Point to next cluster
-	  sector_buffer[offset+0]=(offset/4)+1;
-	  sector_buffer[offset+1]=0; sector_buffer[offset+2]=0; sector_buffer[offset+3]=0;	  
-	}
-      } else {
-	if (start_cluster) {
-	  write_line("ERROR: Disk space is fragmented. File not created.",0);
-	  return 0;
+	  if (start_cluster) {
+	    // write_line("ERROR: Disk space is fragmented. File not created.",0);
+	    // 	    return 0;
+	    // Not enough contiguous space in this FAT sector, so try the next
+	    break;
+	  }
 	}
       }
+
+    if (start_cluster&&(contiguous_clusters==clusters)) break;
+    else {
+      printf("FAT sector #%d : start_cluster=%d, contigous_clusters=%d\n",
+	     fat_offset,start_cluster,contiguous_clusters);
     }
+  }
   if ((!start_cluster)||(contiguous_clusters!=clusters)) {
-    write_line("ERROR: Could not find enough free clusters early in file system",0);
+    write_line("ERROR: Could not find enough free clusters in file system",0);
     return 0;
   }
+
+  
 #ifdef __CC65__
   write_line("First free cluster is ",0);
   screen_decimal(screen_line_address-80+22,start_cluster);
@@ -107,6 +136,7 @@ long fat32_create_contiguous_file(char *name, long size,
   sector_buffer[offset+0x1F]=(size>>24l)&0xff;
 
   sdcard_writesector(root_dir_sector);
-  
+
+  printf("Found space starting at cluster %d\n",start_cluster);
   return root_dir_sector+(start_cluster-2)*8;
 }
