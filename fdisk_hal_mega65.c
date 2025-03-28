c0692564750228843370f34afafe1433a73f0b84
Gurce Isikyildiz <gurce.isikyildiz@gmail.com>
2025-03-28 01:35:30 +0100

    (#26) only use workaround for mega65r3

--- fdisk_hal_mega65.c
+++ fdisk_hal_mega65.c
@@ -276,7 +276,9 @@ void sdcard_readsector(const uint32_t sector_number)
 void flash_read512bytes(const uint32_t byte_offset)
 {
   // Hard-coding latency cycle thing to suit MEGA65R3 QSPI
-  POKE(sd_ctl, 0x5f);
+  if (PEEK(0xD629) == 0x03) { // only do this for mega65r3
+    POKE(sd_ctl, 0x5f);
+  }
 
   do_read_sector(0x53, byte_offset);
 }
